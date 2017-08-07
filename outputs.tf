10dce32eb7ad49f4422040477430847e07976a5d
Erik Osterman <erik@cloudposse.com>
2017-08-06 23:05:59 -0700

    add zone_name for consistency

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,10 @@ output "zone_id" {
   value = "${aws_route53_zone.default.zone_id}"
 }
 
+output "zone_name" {
+  value = "${aws_route53_zone.default.name}"
+}
+
 output "fqdn" {
   value = "${aws_route53_zone.default.name}"
 }
