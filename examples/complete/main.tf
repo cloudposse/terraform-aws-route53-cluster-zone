bd7b562db48b6e3c92ad229a7232037f9db9b0ae
aknysh <andriy.knysh@gmail.com>
2019-06-24 11:19:45 -0400

    Update README and example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -5,4 +5,11 @@ module "domain" {
   name             = var.name
   parent_zone_name = var.parent_zone_name
   zone_name        = "$${name}.$${parent_zone_name}"
+
+  providers = {
+    aws      = "aws"
+    null     = "null"
+    local    = "local"
+    template = "template"
+  }
 }
