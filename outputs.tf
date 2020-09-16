bd7a8766c2e4330c2058122a8b0e8b17f5b55e52
Matt Gowie <matt@masterpoint.io>
2020-09-15 21:52:06 -0600

    Adds zone_name_servers output

--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,11 @@ output "zone_name" {
   description = "Route53 DNS Zone name"
 }
 
+output "zone_name_servers" {
+  value       = try(aws_route53_zone.default[0].name_servers, [])
+  description = "Route53 DNS Zone Name Servers"
+}
+
 output "fqdn" {
   value       = join("", aws_route53_zone.default.*.name)
   description = "Fully-qualified domain name"
