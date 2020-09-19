79495d4c6af67921c4fad5fa430d9300b48fe76b
Matt Gowie <matt@masterpoint.io>
2020-09-19 09:37:01 -0600

    Upgrades repo to context.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,9 @@
-module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
-  namespace   = var.namespace
-  stage       = var.stage
-  environment = var.environment
-  name        = var.name
-  delimiter   = var.delimiter
-  attributes  = var.attributes
-  tags        = var.tags
-  enabled     = var.enabled
-}
-
 locals {
-  enabled                    = var.enabled ? 1 : 0
-  parent_zone_record_enabled = var.parent_zone_record_enabled && var.enabled ? 1 : 0
+  enabled                    = module.this.enabled ? 1 : 0
+  parent_zone_record_enabled = var.parent_zone_record_enabled && module.this.enabled ? 1 : 0
 }
 
-data "aws_region" "default" {
-}
+data "aws_region" "default" {}
 
 data "aws_route53_zone" "parent_zone" {
   count   = local.parent_zone_record_enabled
@@ -29,12 +16,12 @@ data "template_file" "zone_name" {
   template = replace(var.zone_name, "$$", "$")
 
   vars = {
-    namespace        = var.namespace
-    environment      = var.environment
-    name             = var.name
-    stage            = var.stage
-    id               = module.label.id
-    attributes       = join(var.delimiter, module.label.attributes)
+    namespace        = module.this.namespace
+    environment      = module.this.environment
+    name             = module.this.name
+    stage            = module.this.stage
+    id               = module.this.id
+    attributes       = join(var.delimiter, module.this.attributes)
     parent_zone_name = coalesce(join("", data.aws_route53_zone.parent_zone.*.name), var.parent_zone_name)
     region           = data.aws_region.default.name
   }
@@ -43,7 +30,7 @@ data "template_file" "zone_name" {
 resource "aws_route53_zone" "default" {
   count = local.enabled
   name  = join("", data.template_file.zone_name.*.rendered)
-  tags  = module.label.tags
+  tags  = module.this.tags
 }
 
 resource "aws_route53_record" "ns" {
