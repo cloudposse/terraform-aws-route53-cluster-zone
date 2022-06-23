249b7fd917343a5a7fcbba616934c671a972eb5f
Kevin Mahoney <kevin@icecube.dog>
2022-06-23 17:00:25 -0600

    actually add tests

---
+++ test/src/examples_without_parent_zone_test.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+// Test the Terraform module in examples/without_parent_zone using Terratest.
+func TestExamplesWithoutParentZone(t *testing.T) {
+	t.Parallel()
+
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../../examples/without_parent_zone",
+		Upgrade:      true,
+		// Variables to pass to our Terraform code using -var-file options
+		VarFiles: []string{"fixtures.us-east-2.tfvars"},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the value of an output variable
+	zoneName := terraform.Output(t, terraformOptions, "zone_name")
+
+	expectedZoneName := "testing.cloudposse.co"
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, expectedZoneName, zoneName)
+}
